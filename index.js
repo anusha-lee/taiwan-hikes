94b0846e9ad3fe5bfe2f82af777764a3008e009b
Anusha Lee <anushalee@gmx.com>
2019-11-20 09:42:02 +0800

    fix scroll down function. and 7 reasons page

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // fixed navigation bar after scrolling down
 window.addEventListener("scroll", function() {
-  const navFixed = document.querySelector("header-box");
+  const navFixed = document.querySelector(".header-box");
   const fromTop = window.scrollY;
-  console.log(navFixed);
-  if (fromTop > 450) {
+  if (fromTop > 500) {
     navFixed.classList.add("fixedNav");
   } else {
     navFixed.classList.remove("fixedNav");
