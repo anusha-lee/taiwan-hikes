3d99a29eab78c5e360a72cbf8389ceb91336f72e
Anusha Lee <anushalee@gmx.com>
2019-11-22 01:13:07 +0800

    work on image carousel. not done yet.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // fixed navigation bar after scrolling down
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", () => {
   const navFixed = document.querySelector(".header-box");
   const fromTop = window.scrollY;
   if (fromTop > 500) {
