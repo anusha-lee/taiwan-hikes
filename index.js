82fba7c4f8a13430fa9c248e9463ead6858a9738
Anusha Lee <anushalee@gmx.com>
2019-11-29 16:50:42 +0800

    add scroll up button, but need to fix the function

--- index.js
+++ index.js
@@ -9,6 +9,38 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// scroll up button
+
+// window.addEventListener("scroll", function scrollUp() {
+//   const scrollDown =
+//     document.documentElement.scrollTop + document.body.scrollTop;
+//   const scrollButton = document.querySelector(".scroll-up");
+//   if (scrollDown > 30) {
+//     scrollButton.style.display = "block";
+//   } else {
+//     scrollButton.style.display = "none";
+//   }
+// });
+
+window.addEventListener("scroll", function showScroll() {
+  const top = window.scrollY;
+  if (top > 100) {
+    document.querySelector(".scroll-up").style.display = "block";
+  } else {
+    document.querySelector(".scroll-up").style.display = "none";
+  }
+});
+
+const scrollToTopBtn = document.querySelector(".scroll-up");
+const container = document.querySelector(".head-box");
+console.log(container);
+click.addEventListener("click", function() {
+  container.scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+});
+
 // Page scroll indicator
 // window.onscroll = function() {
 //   function scrollIndicator() {
