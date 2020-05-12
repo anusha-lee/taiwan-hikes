c8eb438216f2c86bf6c705207492d2de5815b61f
Anusha Lee <anushalee@gmx.com>
2020-05-13 07:38:55 +0800

    try to write unit converter function

--- js/converter.js
+++ js/converter.js
@@ -1,3 +1,4 @@
+// Make tabs for user to switch the unit converters
 const tabs = Array.from(
   document.querySelectorAll(".converter__tabs--singleTab")
 );
@@ -22,3 +23,22 @@ tabs.forEach((tab) => {
     //console.log(tabContent);
   });
 });
+
+// Calculate the inputs
+// get the input and make sure the inputs are numbers
+let inputMeters = document.querySelector("#meters").value;
+let inputFeet = document.querySelector("#feet").value;
+console.log(inputMeters);
+function calculateLength(inputMeters) {
+  let resultFeet = (document.querySelector("#feet").value =
+    inputMeters * 3.2808);
+  console.log(typeof resultFeet);
+}
+
+// function calculateFeet() {
+
+// }
+
+// function calculateCelsius() {
+
+// }
