ed6b8773d3c340b881cb6c4dfd5777ee37129260
Anusha Lee <anushalee@gmx.com>
2020-07-09 23:24:42 +0800

    fix some hiking difficulty level

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/a2ebf972d72194b5099f40bac769a5d18e3438a1/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/d000365c53dc38aec6845b60e3b66b8149c41f57/trips.json"
   );
   //    let response = await fetch('https://gist.githubusercontent.com/madaf/3934fad7205adfb3abdb7b79737898f0/raw/db29239d6205807b55ea4dc372fa5ff01bf57b0e/trips.json');
   guidedTrips = await response.json();
