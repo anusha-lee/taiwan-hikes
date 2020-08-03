10f5a97997d6b86992caa8d2d7d60bdf7e0ed8a8
Anusha Lee <anushalee@gmx.com>
2020-08-04 01:18:43 +0800

    update Dudu's trips and fix a broken link

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/cad5c60836c60987a42033a3f4c652834b51b083/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/75147393e03e83465813652d2d93ce65d3b85fe2/trips.json"
   );
   //    let response = await fetch('https://gist.githubusercontent.com/madaf/3934fad7205adfb3abdb7b79737898f0/raw/db29239d6205807b55ea4dc372fa5ff01bf57b0e/trips.json');
   guidedTrips = await response.json();
