faad087bb2af79d278bbc4e5f9f7ec93126a3f68
Anusha Lee <anushalee@gmx.com>
2020-08-04 22:17:54 +0800

    update LOHAS hiking trips and edit Tefuye trail post

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/75147393e03e83465813652d2d93ce65d3b85fe2/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/d7b19c70d17db0a32ce7609871784152ef217b70/trips.json"
   );
   //    let response = await fetch('https://gist.githubusercontent.com/madaf/3934fad7205adfb3abdb7b79737898f0/raw/db29239d6205807b55ea4dc372fa5ff01bf57b0e/trips.json');
   guidedTrips = await response.json();
