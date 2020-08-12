a7b2fbbb0baaf5ad984015e880035ae164e167b4
Anusha Lee <anushalee@gmx.com>
2020-08-13 02:07:06 +0800

    update XingXin hiking trips, add the header image of Smangus, try to fix the spaces in posts of Tefuye and Data, but still can't get rid of them

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/d7b19c70d17db0a32ce7609871784152ef217b70/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/f1633338d30bf8eb586c6f9f7b362b1929d232e1/trips.json"
   );
   //    let response = await fetch('https://gist.githubusercontent.com/madaf/3934fad7205adfb3abdb7b79737898f0/raw/db29239d6205807b55ea4dc372fa5ff01bf57b0e/trips.json');
   guidedTrips = await response.json();
