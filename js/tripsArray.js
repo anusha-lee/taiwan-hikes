243a8548b759a30a05a44c8d1d987a5ac5c8980b
Anusha Lee <anushalee@gmx.com>
2020-08-21 07:06:19 +0800

    update trips and work on flowers page

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/f1633338d30bf8eb586c6f9f7b362b1929d232e1/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/52d4c7b399873f61009e9be4866486e4b5831948/trips.json"
   );
   //    let response = await fetch('https://gist.githubusercontent.com/madaf/3934fad7205adfb3abdb7b79737898f0/raw/db29239d6205807b55ea4dc372fa5ff01bf57b0e/trips.json');
   guidedTrips = await response.json();
