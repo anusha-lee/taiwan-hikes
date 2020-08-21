6949dbcb43fb681897403e97b5d031c6bd2b04ba
Anusha Lee <anushalee@gmx.com>
2020-08-21 17:40:21 +0800

    upload Smangus post, update on hiking trip

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/52d4c7b399873f61009e9be4866486e4b5831948/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/892778fd4a62f7a6f2fbb2986583dd23ff6f89ad/trips.json"
   );
   //    let response = await fetch('https://gist.githubusercontent.com/madaf/3934fad7205adfb3abdb7b79737898f0/raw/db29239d6205807b55ea4dc372fa5ff01bf57b0e/trips.json');
   guidedTrips = await response.json();
