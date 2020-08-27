d269110aacc9f982ce259b91e004d32a40803feb
Anusha Lee <anushalee@gmx.com>
2020-08-28 01:07:04 +0800

    work on the layout of Flowers page. Edit Smangus post

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/892778fd4a62f7a6f2fbb2986583dd23ff6f89ad/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/805dd86ed740f57377c87c603036cb0991abf7ad/trips.json"
   );
   //    let response = await fetch('https://gist.githubusercontent.com/madaf/3934fad7205adfb3abdb7b79737898f0/raw/db29239d6205807b55ea4dc372fa5ff01bf57b0e/trips.json');
   guidedTrips = await response.json();
