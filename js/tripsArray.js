b3a5d4bc1989f9691755995466f4b7c3c30c7a10
Anusha Lee <anushalee@gmx.com>
2020-09-07 00:08:06 +0800

    update About photo and guided trips

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/35e191a9e433e4f3408fae0a1e72bb62eb3b2636/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/b36af8d004bdac6647be4c64b42bfe8e611a8fbd/trips.json"
   );
   //    let response = await fetch('https://gist.githubusercontent.com/madaf/3934fad7205adfb3abdb7b79737898f0/raw/db29239d6205807b55ea4dc372fa5ff01bf57b0e/trips.json');
   guidedTrips = await response.json();
