79467b5561ab4e2c0d7f9c2129e049a949d26fb8
Anusha Lee <anushalee@gmx.com>
2020-09-14 22:37:51 +0800

    update guided trips and work on search functions on Flowers page

--- js/searchFlowers.js
+++ js/searchFlowers.js
@@ -1,20 +1,19 @@
 const flowerColors = document.querySelector("#flower-colors");
 const flowerSpecies = document.querySelector("#flower-species");
-const flowerSpecialInterests = document.querySelector(
-  "#flower-special-interests"
-);
+const flowerSpecialInterests = document.querySelector("#flower-interests");
+
 const flowerColorChildren = [...flowerColors.children];
 
 const flower = document.querySelector(".flower");
 const originalFlowerContent = flower.innerHTML;
 const flowerChildren = [...flower.children];
 const flowerSearchResults = document.querySelector(".flower-search-results");
-const flowerInfo = flower.querySelector(".flower__info");
-console.log(flowerSearchResults);
+
+// try to search through .
+const flowerInfo = [...flower.querySelectorAll(".flower__info")];
 
 // For Show All Flowers button
 const showAllFlowersBtn = document.querySelector(".show-all-flowers-btn");
-console.log(showAllFlowersBtn);
 function showAllFlowersButton() {
   showAllFlowersBtn.addEventListener("click", (event) => {
     flowerSearchResults.innerHTML = "";
@@ -26,19 +25,56 @@ function showAllFlowersButton() {
 flowerColors.addEventListener("change", (event) => {
   // clear up the flowers
   flower.innerHTML = "";
-  showAllFlowersButton();
+  flowerSearchResults.innerHTML = "";
   const searchColorValue = event.target.value;
 
-  flowerColorChildren.forEach((flowerColorChild) => {
-    const flowerBoxColor = flowerColorChild.getAttribute("data-color");
+  // search color
+  flowerInfo.forEach((eachFlowerInfo) => {
+    const flowerInfoColor = eachFlowerInfo.getAttribute("data-color");
 
-    // need to append the parent of flowerBoxColor
-    // if (flowerBoxColor === searchColorValue) {
-    //   flowerSearchResults.appendChild(flowerBoxColor);
-    // }
+    // Color: need to append the right dataset of eachFlowreInfo to flowerSearchResults
+    if (flowerInfoColor === searchColorValue) {
+      flowerSearchResults.appendChild(eachFlowerInfo);
+    }
   });
+  showAllFlowersButton();
 });
 
-//console.log(flowerSearchResults);
+// Search flower species
+flowerSpecies.addEventListener("change", (event) => {
+  // clear up the flowers
+  flower.innerHTML = "";
+  flowerSearchResults.innerHTML = "";
+  const searchSpeciesValue = event.target.value;
 
-// showAllFlowersButton();
+  // search species
+  flowerInfo.forEach((eachFlowerInfo) => {
+    const flowerInfoSpecies = eachFlowerInfo.getAttribute("data-species");
+
+    // Species: need to append the right dataset of eachFlowreInfo to flowerSearchResults
+    if (flowerInfoSpecies === searchSpeciesValue) {
+      flowerSearchResults.appendChild(eachFlowerInfo);
+    }
+  });
+  showAllFlowersButton();
+});
+
+// Search flower interests
+flowerSpecialInterests.addEventListener("change", (event) => {
+  // clear up the flowers
+  flower.innerHTML = "";
+  flowerSearchResults.innerHTML = "";
+  const searchInterestValue = event.target.value;
+  console.log(searchInterestValue);
+
+  // search species
+  flowerInfo.forEach((eachFlowerInfo) => {
+    const flowerInfoInterest = eachFlowerInfo.getAttribute("data-interests");
+
+    // Species: need to append the right dataset of eachFlowreInfo to flowerSearchResults
+    if (flowerInfoInterest === searchInterestValue) {
+      flowerSearchResults.appendChild(eachFlowerInfo);
+    }
+  });
+  showAllFlowersButton();
+});
