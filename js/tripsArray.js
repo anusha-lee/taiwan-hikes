38103acbd941e6e27c3c87ceea2b825215377dd5
Anusha Lee <anushalee@gmx.com>
2020-09-16 23:20:38 +0800

    edit some text on Blog page, add 2 flowers on Flowers page

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/414cf98b29e7f07be4ff3aab379b817e80350204/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/9d0d1d832286fae125a7094e6cebdff0d1acddf7/trips.json"
   );
   //    let response = await fetch('https://gist.githubusercontent.com/madaf/3934fad7205adfb3abdb7b79737898f0/raw/db29239d6205807b55ea4dc372fa5ff01bf57b0e/trips.json');
   guidedTrips = await response.json();
