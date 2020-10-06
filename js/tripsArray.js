8c4bd5373f6f295388ec1416f311cd814ebc1751
Anusha Lee <anushalee@gmx.com>
2020-10-07 00:43:58 +0800

    update LOHAS and DanSanLer guided trips

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/785456ceb3dcde6ab4678b829d05a5642d070b71/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/7295d2c0cca4ca6a1e2223f85a4170d9c5f40b69/trips.json"
   );
   //    let response = await fetch('https://gist.githubusercontent.com/madaf/3934fad7205adfb3abdb7b79737898f0/raw/db29239d6205807b55ea4dc372fa5ff01bf57b0e/trips.json');
   guidedTrips = await response.json();
