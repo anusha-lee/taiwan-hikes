b614e3384612e51eb715d6485087113dd3643ab1
Anusha Lee <anushalee@gmx.com>
2020-11-04 12:24:48 +0800

    update guided hiking trips

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,9 +2,8 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/e21855419471b5f3c1155927f1f60321d977dd66/trips.json"
-  );
-  //    let response = await fetch('https://gist.githubusercontent.com/madaf/3934fad7205adfb3abdb7b79737898f0/raw/db29239d6205807b55ea4dc372fa5ff01bf57b0e/trips.json');
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/5c520f1eda066014da043fc01c7bb7c05d339129/trips.json"
+  );  
   guidedTrips = await response.json();
   return guidedTrips;
 }
