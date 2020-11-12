61a0c46fb891ba01c9b6a2d07e14ee7665bfc832
Anusha Lee <anushalee@gmx.com>
2020-11-13 00:38:59 +0800

    Mt. Yuan post

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/5c520f1eda066014da043fc01c7bb7c05d339129/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/17bae07b1daef73af8cb70b63e60526066bf4c9d/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
