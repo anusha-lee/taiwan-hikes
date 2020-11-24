7ee02fe55446d6ce8c815a66f5408be5f962e03c
Anusha Lee <36042823+anusha-lee@users.noreply.github.com>
2020-11-25 00:11:22 +0800

    add a new trip from LHOS

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/17bae07b1daef73af8cb70b63e60526066bf4c9d/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/18413cdfa113afd2036006beb542a43cfcda85f1/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
