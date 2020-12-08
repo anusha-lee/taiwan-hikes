0f5d865f58b52971f208c439d71d06d82dfda0b9
anusha-lee <anushalee@gmx.com>
2020-12-08 23:38:54 +0800

    update latest hiking trips

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/18413cdfa113afd2036006beb542a43cfcda85f1/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/686d654d0f2d724cf1e5e3eee48bf10a3ed74c37/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
