07ae5acc28366ac07dbef835f81d6562e03ec252
anusha-lee <anushalee@gmx.com>
2020-12-11 00:38:47 +0800

    add images on the post of Mt. Baigu

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/686d654d0f2d724cf1e5e3eee48bf10a3ed74c37/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/8d61bbb17838d79af40e1363afbf4c98c5184c35/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
