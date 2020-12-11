60f3753401cb6f8a90bbd9407092c35e324eb432
anusha-lee <anushalee@gmx.com>
2020-12-11 18:50:53 +0800

    edit Baigu post

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/8d61bbb17838d79af40e1363afbf4c98c5184c35/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/a4e43d2dde9c29da689994614d3480be21d177cd/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
