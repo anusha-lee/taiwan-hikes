f157af99daa89f5d20ee1e3b9561071ebc8d831b
anusha-lee <anushalee@gmx.com>
2020-12-25 03:14:46 +0800

    add Sanchong group hikes

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/ba1cef844b7b1402d7a742eb0cd25cb671427705/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/82eb10c5e83cdd38630bf876269d6eab2ebbd411/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
