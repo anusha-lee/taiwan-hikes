d391d143e935b0657df563d8a8562a7ccd492066
anusha-lee <anushalee@gmx.com>
2021-01-08 01:02:47 +0800

    update guided trips, and add new post of Dabajian

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/b377c2cf8c22dd8acfd494ba7967b8b5ca8207dd/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/ec07bd4a4e12d9a1d7cc74c837a55452f978a4ca/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
