b35d274e0e2aa2e5cf451dc58acd71d49c9927c8
anusha-lee <anushalee@gmx.com>
2021-01-22 00:28:47 +0800

    Puli 2 post and add images

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/ec07bd4a4e12d9a1d7cc74c837a55452f978a4ca/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/5657ae2b24e1dea7cc76ae95f520ccec1892025f/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
