4a27e02f62b8e1bd21db9fd245b20257636d6ab3
anusha-lee <anushalee@gmx.com>
2021-03-23 01:18:17 +0800

    update Xizhi hiking trips, edit ROMA hike

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/194125dd8dc2592ddb73419e46a7e8e59bb0bd36/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/eb96f9888c96462b11818c48bc6d2528ef0ec6ce/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
