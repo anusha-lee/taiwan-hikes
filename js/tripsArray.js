9910b5cecb9a52d7374458095bd74739f76d6641
anusha-lee <anushalee@gmx.com>
2021-03-26 02:47:09 +0800

    delete repeated guided hike and add Syakaro post

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/eb96f9888c96462b11818c48bc6d2528ef0ec6ce/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/83a4955b3b26a6018e86447bff5a7e931dc0eed6/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
