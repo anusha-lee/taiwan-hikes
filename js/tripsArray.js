d1654089c3449f89d48bec5b0fd5d2e63cfc699f
anusha-lee <anushalee@gmx.com>
2021-04-08 17:26:49 +0800

    put Sanzhong, LOHAS, THMA trips on guided hikes page

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/3aa303bd76dff97b1277fd17677f1fe2ab6191c3/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/558a94da7e5c3f5dc44bf5609b870c8ea6a94242/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
