4cc17ba2d0110686d21c9f25d887902fe8b99ead
anusha-lee <anushalee@gmx.com>
2021-04-09 23:01:17 +0800

    update and proofread Bitou Cape post

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/558a94da7e5c3f5dc44bf5609b870c8ea6a94242/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/018526f46d8f00604c57ade7144045b409e308a4/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
