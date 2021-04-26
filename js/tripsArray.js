9dc1cdb0121dec05986029ecf841f2c89c18253e
anusha-lee <anushalee@gmx.com>
2021-04-27 00:34:41 +0800

    edit Mt. Baigu to Day 2

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/018526f46d8f00604c57ade7144045b409e308a4/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/4139d806c4389fce01eb81ac58e7d985061a3573/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
