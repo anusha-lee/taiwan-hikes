9914739c0472d5255e9e5725e0147d04ef7cbc1b
anusha-lee <anushalee@gmx.com>
2021-04-28 12:11:14 +0800

    remove Sanchong trips

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/4139d806c4389fce01eb81ac58e7d985061a3573/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/ef18008ba5dd8880792c25aa308f06d6d4e427a4/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
