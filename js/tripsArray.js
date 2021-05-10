bf4b57431115c966bb68814b3cc33a5fb8edf46c
anusha-lee <anushalee@gmx.com>
2021-05-10 23:27:06 +0800

    update Xinxing hiking trips and edit Mt. Keelung post again

--- js/tripsArray.js
+++ js/tripsArray.js
@@ -2,7 +2,7 @@ let guidedTrips = [];
 
 async function fetchGuidedTrips() {
   let response = await fetch(
-    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/c70e4f8dd66ee6597af3c2466e0b1d92d2fe7514/trips.json"
+    "https://gist.githubusercontent.com/anusha-lee/fb0d5987943b7044745131c142bd6ebb/raw/63a7980563a0cd31c6eab423763da086021bdba1/trips.json"
   );  
   guidedTrips = await response.json();
   return guidedTrips;
